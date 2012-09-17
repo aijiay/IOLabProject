f114be762d3b86e25854a312b421b7d2078ab0d5
Qianqian Zhao <chance.zhao@gmail.com>
2012-09-17 14:10:42 -0800

    verification working, removed bootstrap

--- login.js
+++ login.js
@@ -2,14 +2,19 @@
 
 
 		$(document).ready(function() {
-				$('#login-form').submit(function(){
+				$('#login-form').on("submit", function(){
 					delicious.username = $('#username').val();
 					delicious.password = $('#password').val();
 					console.log("Login clicked");
 					saveLogin();
+					return false;
 				});
 
 				function saveLogin(){
+
+					console.log(delicious.username);
+					console.log(delicious.password);
+					console.log("U/P printed inside saveLogin()");
 
 					var postData = {
 						method: 'posts/get',
@@ -21,19 +26,20 @@
                     			$.getJSON('https://people.ischool.berkeley.edu/~qqz/delicious_proxy.php?callback=?',
 						postData,
                      				function(rsp){
+							alert(rsp.result_code);
                         			//	window.open("page2.html", "_self");
-                    if (rsp.result_code === "access denied") {
-                        alert('The provided Delicious username and password are incorrect.');
-                    } else if (rsp.result_code === "something went wrong") {
-                        alert('There was an unspecified error communicating with Delicious.');
-                    } else if (rsp.result_code === "done") {
-                        // Bookmark was saved properly
-                        	alert("Success!");
-                        } else {
-                            // We're done saving the trail
-                            alert ("Your trail has been saved!");
-                        }
-                    });
+                   					 if (rsp.result_code === "access denied") {
+                       						 alert('The provided Delicious username and password are incorrect.');
+                   					 } else if (rsp.result_code === "something went wrong") {
+                       						 alert('There was an unspecified error communicating with Delicious.');
+                  					  } else if (rsp.result_code === "done") {
+                        					// Bookmark was saved properly
+                        					alert("Success!");
+                       					 } else {
+                            					// We're done saving the trail
+                           					 alert ("Your trail has been saved!");
+                       					 }
+                    				});
 					
 					return false;
 				
