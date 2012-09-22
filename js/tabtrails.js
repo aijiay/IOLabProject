02a10fa4f698442d83f095b3e8a585ed7e4653c0
Qianqian Zhao <chance.zhao@gmail.com>
2012-09-21 21:33:58 -0800

    tab trails login working

--- js/tabtrails.js
+++ js/tabtrails.js
@@ -1,4 +1,4 @@
-var delicious={username: 'ruidai', password: 'iolab1234'};
+var delicious={};
 var tabsData = [];
 
 $(document).on("ready", function() {
@@ -32,6 +32,46 @@ $('#initial-btn').on('click', function() {
 
 }); // end of initial-btn click function
 
+$('#submit-login').on('click', function(){
+	delicious.username = $('#username').val();
+	delicious.password = $('#password').val();
+	checkLogin();
+	return false;
+});
+
+
+function checkLogin(){
+
+	console.log(delicious.username);
+	console.log(delicious.password);
+	console.log("U/P printed inside saveLogin()");
+
+	var postData = {
+		method: 'posts/get',
+                username: delicious.username,
+                password: delicious.password
+        };
+
+       $.getJSON('https://people.ischool.berkeley.edu/~qqz/delicious_proxy.php?callback=?',
+						postData,
+                     				function(rsp){
+                   					 if (rsp.result_code === "access denied") {
+                       						 alert('The provided Delicious username and password are incorrect.');
+                   					 } else if (rsp.result_code === "something went wrong") {
+                       						 alert('There was an unspecified error communicating with Delicious.');
+                  					 
+                       					 } else {
+                            					// We're ok - continue with the login;
+								checkLoginComplete();
+                       					 }
+                    				});
+				
+} // end of checkLogin
+
+function checkLoginComplete(){
+	$('#login-page').hide();
+	$('#enter-trail-name').show();
+}
 
 		/* var newTrailName = 'trail:' + trailname.toLowerCase().replace(/ /g, '_');
 
@@ -53,4 +93,4 @@ $('#initial-btn').on('click', function() {
 	
 
 
-});
+}); // end of document-on-ready
